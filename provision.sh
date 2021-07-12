68b784f5a2f0af35fb73b8f22b009ca027127df2
vagrant <vagrant@vagrant.vm>
2021-07-12 12:54:15 +0000

    2-initial-django

--- provision.sh
+++ provision.sh
@@ -48,6 +48,27 @@ sudo mysql -u root << EOF
 EOF
 # fi
 
+# superuser 名字
+USER='admin'
+# superuser 密码
+PASS='admin'
+# superuser 邮箱
+MAIL='admin@twitter.com'
+script="
+from django.contrib.auth.models import User;
+
+username = '$USER';
+password = '$PASS';
+email = '$MAIL';
+
+if not User.objects.filter(username=username).exists():
+  User.objects.create_superuser(username, email, password);
+  print('Superuser created.')
+else:
+  print('Superuser creation skipped.');
+
+"
+printf "$script" | python manage.py shell
 
 # 如果想直接进入/vagrant路径下
 # 请输入vagrant ssh命令进入
