beb57a1e8b8b40805ac247fab2612cb53b3766ba
vagrant <vagrant@vagrant.vm>
2021-07-19 02:22:35 +0000

    21-install-notfications-api

--- likes/api/views.py
+++ likes/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from inbox.services import NotificationService
 from likes.api.serializers import (
     LikeSerializer,
     LikeSerializerForCancel,
@@ -17,7 +18,7 @@ class LikeViewSet(viewsets.GenericViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = LikeSerializerForCreate
 
-    @required_params(request_attr='data', params=['content_type', 'object_id'])
+    @required_params(method='POST', params=['content_type', 'object_id'])
     def create(self, request, *args, **kwargs):
         serializer = LikeSerializerForCreate(
             data=request.data,
@@ -28,14 +29,16 @@ class LikeViewSet(viewsets.GenericViewSet):
                 'message': 'Please check input',
                 'errors': serializer.errors,
             }, status=status.HTTP_400_BAD_REQUEST)
-        instance = serializer.save()
+        instance, created = serializer.get_or_create()
+        if created:
+            NotificationService.send_like_notification(instance)
         return Response(
             LikeSerializer(instance).data,
             status=status.HTTP_201_CREATED,
         )
 
     @action(methods=['POST'], detail=False)
-    @required_params(request_attr='data', params=['content_type', 'object_id'])
+    @required_params(method='POST', params=['content_type', 'object_id'])
     def cancel(self, request, *args, **kwargs):
         serializer = LikeSerializerForCancel(
             data=request.data,
@@ -45,6 +48,6 @@ class LikeViewSet(viewsets.GenericViewSet):
             return Response({
                 'message': 'Please check input',
                 'errors': serializer.errors,
-            })
+            }, status=status.HTTP_400_BAD_REQUEST)
         serializer.cancel()
         return Response({'success': True}, status=status.HTTP_200_OK)
--- twitter/settings.py
+++ twitter/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # third_party
     'rest_framework',
     'django_filters',
+    'notifications',
 
     # project apps
     'accounts',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
