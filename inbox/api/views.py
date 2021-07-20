4677f99093bfc0d9e239f48fa06cb2ab28e56cc2
vagrant <vagrant@vagrant.vm>
2021-07-20 09:35:51 +0000

    22-notifications-api-part-1

---
+++ inbox/api/views.py
@@ -0,0 +1,29 @@
+from inbox.api.serializers import NotificationSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+
+from inbox.api.serializers import NotificationSerializer
+
+
+class NotificationViewSet(
+    viewsets.GenericViewSet,
+    viewsets.mixins.ListModelMixin,
+):
+    serializer_class = NotificationSerializer
+    permission_classes = (IsAuthenticated,)
+    filterset_fields = ('unread',)
+
+    def get_queryset(self):
+        return self.request.user.notifications.all()
+
+    @action(methods=['GET'], detail=False, url_path='unread-count')
+    def unread_count(self, request, *args, **kwargs):
+        count = self.get_queryset().filter(unread=True).count()
+        return Response({'unread_count': count}, status=status.HTTP_200_OK)
+
+    @action(methods=['POST'], detail=False, url_path='mark-all-as-read')
+    def mark_all_as_read(self, request, *args, **kwargs):
+        update_content = self.get_queryset().update(unread=False)
+        return Response({'marked_count': update_content}, status=status.HTTP_200_OK)
