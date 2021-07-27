910d2e8080e410b99528898c687bb42b0d1afe3e
vagrant <vagrant@vagrant.vm>
2021-07-27 02:42:07 +0000

    27-tweets-pagination

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -10,6 +10,7 @@ from tweets.api.serializers import (
 )
 from tweets.models import Tweet
 from utils.decorators import required_params
+from utils.paginations import EndlessPagination
 
 
 class TweetViewSet(viewsets.GenericViewSet,
@@ -20,6 +21,7 @@ class TweetViewSet(viewsets.GenericViewSet,
     """
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializerForCreate
+    pagination_class = EndlessPagination
 
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
@@ -40,6 +42,7 @@ class TweetViewSet(viewsets.GenericViewSet,
         tweets = Tweet.objects.filter(
             user_id=request.query_params['user_id']
         ).order_by('-created_at')
+        tweets = self.paginate_queryset(tweets)
         serializer = TweetSerializer(
             tweets,
             context={'request': request},
@@ -48,7 +51,7 @@ class TweetViewSet(viewsets.GenericViewSet,
         # 一般来说 json 格式的 response 默认都要用 hash 的格式
         # 一般来说 json 格式的 response 默认都要用 hash 的格式
         # 而不能用 list 的格式(约定俗称)
-        return Response({'tweets': serializer.data})
+        return self.get_paginated_response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         """
