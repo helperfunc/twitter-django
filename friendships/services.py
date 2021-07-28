6ace22800fcdacc191a80ee73f004d523dd21540
vagrant <vagrant@vagrant.vm>
2021-07-28 07:04:24 +0000

    29-add-memcached

--- friendships/services.py
+++ friendships/services.py
@@ -1,4 +1,11 @@
+from django.conf import settings
+from django.core.cache import caches
+
 from friendships.models import Friendship
+from twitter.cache import FOLLOWING_PATTERN
+
+cache = caches['testing'] if settings.TESTING else caches['default']
+
 
 class FriendshipService(object):
 
@@ -8,22 +15,22 @@ class FriendshipService(object):
         # 这种写法会导致 N + 1 Queries 的问题
         # 即 filter 出所有 friendships 耗费了一次 Query
         # 而 for 循环每个 friendship 取 from_user 又耗费了 N 次Queries
-        #friendships = Friendship.objects.filter(to_user=user)
-        #return [friendships.from_user for friendship in friendships]
+        # friendships = Friendship.objects.filter(to_user=user)
+        # return [friendships.from_user for friendship in friendships]
 
         # 错误写法二
         # 这种写法是使用了 Join 操作，让 friendship table 和 user table 在 from_user
         # 这个属性上 join 了起来。 join 操作在大规模用户的 web 场景下是禁用的。因为非常慢。
-        #friendships = Friendship.objects.filter(
+        # friendships = Friendship.objects.filter(
         #    to_user=user,
-        #).select_related('from_user')
-        #return [friendships.from_user for friendship in friendships]
+        # ).select_related('from_user')
+        # return [friendships.from_user for friendship in friendships]
 
         # 正确写法一，自己手动 filter id，使用 in query 查询
-        #from django.contrib.auth.models import User
-        #friendships = Friendship.objects.filter(to_user=user)
-        #follower_ids = [friendship.from_user_id for friendship in friendships]
-        #followers = User.objects.filter(id__in=follower_ids)
+        # from django.contrib.auth.models import User
+        # friendships = Friendship.objects.filter(to_user=user)
+        # follower_ids = [friendship.from_user_id for friendship in friendships]
+        # followers = User.objects.filter(id__in=follower_ids)
 
         # 正确的写法二，使用 prefetch_related, 会自动执行两条语句，用 In Query 查询
         # 实际执行的 SQL 查询和上面是一样的，一共两条 SQL Queries。
@@ -33,8 +40,20 @@ class FriendshipService(object):
         return [friendship.from_user for friendship in friendships]
 
     @classmethod
-    def has_followed(cls, from_user, to_user):
-        return Friendship.objects.filter(
-            from_user=from_user,
-            to_user=to_user,
-        ).exists()
+    def get_following_user_id_set(cls, from_user_id):
+        key = FOLLOWING_PATTERN.format(user_id=from_user_id)
+        user_id_set = cache.get(key)
+        if user_id_set is not None:
+            return user_id_set
+
+        friendships = Friendship.objects.filter(from_user_id=from_user_id)
+        user_id_set = set([
+            fs.to_user_id
+            for fs in friendships
+        ])
+        return user_id_set
+
+    @classmethod
+    def invalidate_following_cache(cls, from_user_id):
+        key = FOLLOWING_PATTERN.format(user_id=from_user_id)
+        cache.delete(key)
--- friendships/tests.py
+++ friendships/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from friendships.models import Friendship
+from friendships.services import FriendshipService
+from testing.testcases import TestCase
 
-# Create your tests here.
+
+class FriendshipServiceTests(TestCase):
+
+    def setUp(self):
+        self.clear_cache()
+        self.user1 = self.create_user('user1')
+        self.user2 = self.create_user('user2')
+
+    def test_get_followings(self):
+        user1 = self.create_user('user_1')
+        user2 = self.create_user('user_2')
+        for to_user in [user1, user2, self.user2]:
+            Friendship.objects.create(from_user=self.user1, to_user=to_user)
+        FriendshipService.invalidate_following_cache(self.user1.id)
+
+        user_id_set = FriendshipService.get_following_user_id_set(self.user1.id)
+        self.assertEqual(user_id_set, {user1.id, user2.id, self.user2.id})
+
+        Friendship.objects.filter(from_user=self.user1, to_user=self.user2).delete()
+        FriendshipService.invalidate_following_cache(self.user1.id)
+        user_id_set = FriendshipService.get_following_user_id_set(self.user1.id)
+        self.assertEqual(user_id_set, {user1.id, user2.id})
