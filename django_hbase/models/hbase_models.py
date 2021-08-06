a5c90b6aa47ab67ebed30248c95c095babe7f9d1
vagrant <vagrant@vagrant.vm>
2021-08-06 03:29:41 +0000

    46-newsfeed-hbase-model

--- django_hbase/models/hbase_models.py
+++ django_hbase/models/hbase_models.py
@@ -2,14 +2,7 @@ from django.conf import settings
 
 from django_hbase.client import HBaseClient
 from django_hbase.models import HBaseField, IntegerField, TimestampField
-
-
-class BadRowKeyError(Exception):
-    pass
-
-
-class EmptyColumnError(Exception):
-    pass
+from django_hbase.models.exceptions import BadRowKeyError, EmptyColumnError
 
 
 class HBaseModel:
@@ -133,14 +126,17 @@ class HBaseModel:
             row_data[column_key] = cls.serialize_field(field, column_value)
         return row_data
 
-    def save(self):
+    def save(self, batch=None):
         row_data = self.serialize_row_data(self.__dict__)
         # 如果 row_data 为空，即没有任何 column key values 需要存储 hbase 会直接不存储
         # 这个 row_key, 因此我们可以 raise 一个 exception 提醒调用者，避免存储空值
         if len(row_data) == 0:
             raise EmptyColumnError()
-        table = self.get_table()
-        table.put(self.row_key, row_data)
+        if batch:
+            batch.put(self.row_key, row_data)
+        else:
+            table = self.get_table()
+            table.put(self.row_key, row_data)
 
     @classmethod
     def get(cls, **kwargs):
@@ -150,10 +146,20 @@ class HBaseModel:
         return cls.init_from_row(row_key, row)
 
     @classmethod
-    def create(cls, **kwargs):
+    def create(cls, batch=None, **kwargs):
         instance = cls(**kwargs)
-        instance.save()
+        instance.save(batch=batch)
         return instance
+
+    @classmethod
+    def batch_create(cls, batch_data):
+        table = cls.get_table()
+        batch = table.batch()
+        results = []
+        for data in batch_data:
+            results.append(cls.create(batch=batch, **data))
+        batch.send()
+        return results
 
     @classmethod
     def get_table_name(cls):
--- newsfeeds/models/newsfeed.py
+++ newsfeeds/models/newsfeed.py
@@ -1,3 +1,10 @@
+'''
+Deprecated
+
+user newsfeeds.hbase_models.HBaseNewsFeed instead
+
+'''
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
